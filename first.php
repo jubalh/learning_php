a5ed36644bd29a25881062b0699391e6b9ddac7b
Michael Vetter <g.bluehut@gmail.com>
2014-08-01 12:14:58 +0200

    my first PHP program

---
+++ first.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Learning PHP</title>
+    <meta charset="utf-8" />
+</head>
+<body>
+<?php
+if (!isset($_POST['usrname']))
+{ ?>
+	<form action="first.php" method="post" accept-charset="utf-8">
+		User:</br>
+		<input type="text" name="usrname" id="usrname"></br>
+		Password:</br>
+		<input type="text" name="password" id="password">
+		<p><input type="submit" value="Continue →"></p>
+	</form>
+<?php
+} else {
+	echo "Hello " . $_POST['usrname'];
+}?>
+
+</body>
+</html>
