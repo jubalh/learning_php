23c9474829ae8d3711233a73cf12ead2cbc0cd0d
Michael Vetter <g.bluehut@gmail.com>
2014-08-07 13:28:14 +0200

    Using PHP Templates for welcome
    Seperate view from control.

---
+++ welcome/form.html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Learning PHP</title>
+    <meta charset="utf-8" />
+</head>
+<body>
+	<form action="" method="post" accept-charset="utf-8">
+		User:</br>
+		<input type="text" name="usrname" id="usrname"></br>
+		Password:</br>
+		<input type="text" name="password" id="password">
+		<p><input type="submit" value="Continue →"></p>
+	</form>
+</body>
+</html>
--- welcome/index.php
+++ welcome/index.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Learning PHP</title>
-    <meta charset="utf-8" />
-</head>
-<body>
 <?php
-if (!isset($_POST['usrname']))
-{ ?>
-	<form action="first.php" method="post" accept-charset="utf-8">
-		User:</br>
-		<input type="text" name="usrname" id="usrname"></br>
-		Password:</br>
-		<input type="text" name="password" id="password">
-		<p><input type="submit" value="Continue →"></p>
-	</form>
-<?php
-} else {
+if (isset($_POST['usrname'])) {
 	echo "Hello " . $_POST['usrname'];
+} else {
+	include 'form.html.php';
 }?>
-
-</body>
-</html>
