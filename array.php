bbd9e8fe566385fb649673c8430c6f9d74219f0e
Michael Vetter <g.bluehut@gmail.com>
2014-08-07 13:15:20 +0200

    array test

---
+++ array.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>title</title>
+</head>
+<body>
+	<?php
+		$t[] = "Hi";
+		$t[] = "You";
+		$t[] = "he";
+		$t['ab'] = "!";
+		$t[3] = "?";
+		print_r($t);
+
+		echo "</br>" . time();
+		echo "</br>" . date(DATE_RSS);
+	?>
+</body>
+</html>
